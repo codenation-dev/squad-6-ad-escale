cd2eeea1d97919b286154f8f1e7cd43bf6f450b9
clecio <clecio.almeida@harpio.com.br>
2019-08-23 01:01:18 -0300

    add: params to show pets info

--- front/src/components/Post.js
+++ front/src/components/Post.js
@@ -1,24 +1,24 @@
 import React from 'react'
 
-const Post = ({ number }) => {
-    return (
-        <div className="col-lg-4 col-6 mb-4 shuffle-item" data-groups="[&quot;design&quot;,&quot;illustration&quot;]">
-            <div className="position-relative inner-box">
-                <div className="image position-relative ">
-                    <img src={`images/pets/${number}.jpg`} alt="portfolio-image" className="img-fluid w-100 d-block" />
-                    <div className="overlay-box">
-                        <div className="overlay-inner">
-                            <a className="overlay-content" href="">
-                                <h5 className="mb-0">Painting</h5>
-                                <p>Design</p>
-                            </a>
-                        </div>
-                    </div>
-                </div>
+const Post = ({ id, name, state, city, category, number }) => {
+  return (
+    <div className="col-lg-4 col-6 mb-4 shuffle-item">
+      <div className="position-relative inner-box">
+        <div className="image position-relative ">
+          <img src={`images/pets/${number}.jpg`} alt="portfolio-image" className="img-fluid w-100 d-block" />
+            <div className="overlay-box">
+              <div className="overlay-inner">
+                <a className="overlay-content" href={`/postDetail/${id}`}>
+                  <h5 className="mb-0">{name}</h5>
+                  <p>{state} - {city}</p>
+                  <p>{category}</p>
+                </a>
+              </div>
             </div>
         </div>
-
-    )
+      </div>
+    </div>
+  )
 }
 
 export default Post
