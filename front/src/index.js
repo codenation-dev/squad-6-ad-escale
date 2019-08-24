528188de5d4fccb637cf403064b37f25c024132a
clecio <clecio.almeida@harpio.com.br>
2019-08-24 16:40:12 -0300

    resolve conflict

--- front/src/index.js
+++ front/src/index.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import { Provider } from 'react-redux';
 import store from './store'
 import './index.css';
+import './scss/custom.scss';
 
 import MyRouter from "./router/router";
 
